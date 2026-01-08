7940ae7d76e5f0377bd6427ea7caa353af651aa8
Manish Kumar <manish@Manishs-MacBook-Pro.local>
2026-01-08 15:33:24 +0100

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,23 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Header from "./Header"
+import { checkValidData } from "../utils/validate"
 
 const Login = () => {
 
     const [isSignInForm, setIsSignInForm] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const name = useRef(null)
+    const email = useRef(null)
+    const password = useRef(null)
 
     const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm)
+    }
+
+    const handleButtonClick = () => {
+       const message = checkValidData(email.current.value,password.current.value)
+       setErrorMessage(message)
     }
     return (
         <div>
@@ -18,12 +29,18 @@ const Login = () => {
                     alt="Netflix Login Background"
                 />
             </div>
-            <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-75 p-8 rounded text-white w-4/12">
+            <form onSubmit={(e)=> e.preventDefault()} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-75 p-8 rounded text-white w-4/12">
                 <h1 className="font-bold text-3xl py-4">{isSignInForm ? 'Sign In' : 'Sign Up'}</h1>
-                {!isSignInForm && (<input type="email" placeholder="Full Name" className="p-4 my-4 border border-gray-700 rounded w-full bg-gray-700" />)}
-                <input type="email" placeholder="Email Address" className="p-4 my-4 border border-gray-700 rounded w-full bg-gray-700" />
-                <input type="password" placeholder="Password" className="p-4 my-4 border border-gray-700 rounded w-full bg-gray-700" />
-                <button type="submit" className="p-4 my-4 bg-red-600 text-white rounded w-full">{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
+                {!isSignInForm && (<input ref={name} type="email" placeholder="Full Name" className="p-4 my-4 border border-gray-700 rounded w-full bg-gray-700" />)}
+                <input ref={email} type="email" placeholder="Email Address" className="p-4 my-4 border border-gray-700 rounded w-full bg-gray-700" />
+                <input ref={password} type="password" placeholder="Password" className="p-4 my-4 border border-gray-700 rounded w-full bg-gray-700" />
+                {errorMessage && <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>}
+                <button type="submit" 
+                    className="p-4 my-4 bg-red-600 text-white rounded w-full"
+                    onClick={handleButtonClick}
+                >
+                    {isSignInForm ? 'Sign In' : 'Sign Up'}
+                </button>
                 <p className="text-gray-400 cursor-pointer" onClick={toggleSignInForm}>
                     {isSignInForm ? 'New to Netflix? ' : 'Already registered? '}
                     <span className="text-white">
